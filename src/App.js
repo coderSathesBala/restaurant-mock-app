65114ced1c96e82e64a3b0e85489b96e0bc0d02f
sathesbitmaker <sathes352@gmail.com>
2021-02-16 21:37:15 -0500

    AddNewFiles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import Intro from './components/Intro.js'
+import OurMission from './components/OurMission.js'
 <link rel="preconnect" href="https://fonts.gstatic.com"
 href="https://fonts.googleapis.com/css2?family=Arvo:ital@1&display=swap" rel="stylesheet"></link>
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div>
         <Intro />
+        <OurMission />
       </div>
     )
   }
