ff3d448776bcea4da9e4a2080c5fed3899a45369
sathesbitmaker <sathes352@gmail.com>
2021-03-19 23:08:31 -0400

    Updates

---
+++ precache-manifest.a31eb52684ff24fb516050de81c40b98.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ecb2ef443efbf5602c94ec0bb7bcd04",
+    "url": "/restaurant-mock-app/index.html"
+  },
+  {
+    "revision": "4d3b6f27a733b4c2450b",
+    "url": "/restaurant-mock-app/static/css/main.d6e8584f.chunk.css"
+  },
+  {
+    "revision": "ad6cd0b11963c12102b0",
+    "url": "/restaurant-mock-app/static/js/2.5c728427.chunk.js"
+  },
+  {
+    "revision": "5058b53817589f89e74d",
+    "url": "/restaurant-mock-app/static/js/3.80aff27d.chunk.js"
+  },
+  {
+    "revision": "4d3b6f27a733b4c2450b",
+    "url": "/restaurant-mock-app/static/js/main.233c86c7.chunk.js"
+  },
+  {
+    "revision": "862160e1c066c01b4fae",
+    "url": "/restaurant-mock-app/static/js/runtime~main.9159e5cc.js"
+  },
+  {
+    "revision": "52eb5b0cd0ead2c15e9b545231d56b3b",
+    "url": "/restaurant-mock-app/static/media/beerImage1.52eb5b0c.jpg"
+  },
+  {
+    "revision": "617cdf45ab970626d1e0025e67028974",
+    "url": "/restaurant-mock-app/static/media/beerImage2.617cdf45.jpg"
+  },
+  {
+    "revision": "02a7c07cf73048d1e86e7ca3d8978b12",
+    "url": "/restaurant-mock-app/static/media/beerImage3.02a7c07c.jpg"
+  },
+  {
+    "revision": "346f96fe3bbdaa09a3b4b2b16839781c",
+    "url": "/restaurant-mock-app/static/media/beerImage4.346f96fe.jpg"
+  },
+  {
+    "revision": "3060bd7bb24bbe2f5329b732c36a3583",
+    "url": "/restaurant-mock-app/static/media/blueImageDiv.3060bd7b.jpg"
+  },
+  {
+    "revision": "dce750a4bead54e97e37957b9a6dbf01",
+    "url": "/restaurant-mock-app/static/media/foodImage.dce750a4.jpeg"
+  },
+  {
+    "revision": "2e06d92eb5df9fd942dc9f34020f69c1",
+    "url": "/restaurant-mock-app/static/media/menuDisplay1.2e06d92e.jpeg"
+  },
+  {
+    "revision": "7188b7f5f3da56198cc05ea7774a5b99",
+    "url": "/restaurant-mock-app/static/media/menuDisplay2.7188b7f5.jpeg"
+  },
+  {
+    "revision": "c237a4d19dc4ac0ce6ea73d0dfcc4810",
+    "url": "/restaurant-mock-app/static/media/menuDisplay3.c237a4d1.jpeg"
+  },
+  {
+    "revision": "38ee06d5f07cfd1aeb0e8d3671b18551",
+    "url": "/restaurant-mock-app/static/media/menuDisplay4.38ee06d5.jpeg"
+  },
+  {
+    "revision": "b9b35a76ea77f40a8c033dc343da060f",
+    "url": "/restaurant-mock-app/static/media/menuDisplay5.b9b35a76.jpeg"
+  },
+  {
+    "revision": "f4144e6988536dbb98becb03646dcf88",
+    "url": "/restaurant-mock-app/static/media/restaurantIntro.f4144e69.jpeg"
+  },
+  {
+    "revision": "8f23402c53446c3b2ffdbced9bf5cf32",
+    "url": "/restaurant-mock-app/static/media/restaurantLogo.8f23402c.jpg"
+  }
+]);
